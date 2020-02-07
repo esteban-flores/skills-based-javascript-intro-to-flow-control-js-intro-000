8b16523f7ed045f12ff73a37efe73bb55ed1cd0c
Esteban Flores <ebflores88@gmail.com>
2020-02-07 06:02:09 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -45,6 +45,14 @@ function switchAge(age) {
 switch(age){
 case age === 13:
   return "You are a teenager"
+  case age === 13:
+    return "You are a teenager"
+    case age === 13:
+      return "You are a teenager"
+      case age === 13:
+        return "You are a teenager"
+        case age === 13:
+          return "You are a teenager"
 default:
   return "You have an age"
 }
